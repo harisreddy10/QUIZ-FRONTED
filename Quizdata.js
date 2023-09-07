faa0de769e8d0d53c75adfffbb1fd9e5f6012ab7
INDRA SENA REDDY C <126607980+harisreddy10@users.noreply.github.com>
2023-09-07 18:03:44 +0530

    Add files via upload

---
+++ Quizdata.js
@@ -0,0 +1,60 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: "Which city in India is Known 'Silicon' city?",
+        options: ["Mumbai","Hyderabad","Delhi","Bengaluru"],
+        answer:4
+
+    },
+    {
+        question: "Who painted the Mona Lisa?",
+
+        options: ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "none of the above"],
+        answer:1
+    },
+    {
+        question: "Which gas do plants absorb from the atmosphere during photosynthesis?",
+
+      options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "none of the above"],
+      answer: 21
+    },
+    {
+        question: "What is the capital of France?",
+
+options: ["Paris", "London", "Rome", "none of the above"],
+    answer: 1
+    },
+    {
+        question:    "Which planet is known as the 'Red Planet'?",
+
+options: ["Mars", "Venus", "Jupiter", "none of the above"],
+answer: 1
+    },
+    {
+        question:    "Who wrote the play 'Romeo and Juliet'?",
+
+options: ["William Shakespeare", "Charles Dickens", "Jane Austen", "none of the above"],
+answer: 1
+    },
+   
+
+];
